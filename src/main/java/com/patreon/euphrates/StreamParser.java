d02423319ee38a467e292357cc0320e85d4668e1
joshbuddy <josh@patreon.com>
2017-11-01 15:11:17 -0700

    Use string builder

--- src/main/java/com/patreon/euphrates/StreamParser.java
+++ src/main/java/com/patreon/euphrates/StreamParser.java
@@ -43,7 +43,7 @@ public class StreamParser {
       int columnIndex = -1;
       int valueCount = -1;
       int size = 0;
-      String currentValue = null;
+      StringBuilder currentValue = new StringBuilder();
       while (reader.hasNext()) {
         int type = reader.next();
         switch (type) {
@@ -88,8 +88,8 @@ public class StreamParser {
                 break;
               case "field":
                 if (columnIndex != -1) {
-                  currentRow.set(columnIndex, currentValue);
-                  currentValue = null;
+                  currentRow.set(columnIndex, currentValue.toString());
+                  currentValue = new StringBuilder();
                   valueCount--;
                   columnIndex = -1;
                 }
@@ -98,11 +98,7 @@ public class StreamParser {
             break;
           case XMLStreamReader.CHARACTERS:
             if (columnIndex != -1) {
-              if (currentValue == null) {
-                currentValue = reader.getText();
-              } else {
-                currentValue += reader.getText();
-              }
+              currentValue.append(reader.getText());
             }
             break;
         }
